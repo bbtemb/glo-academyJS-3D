40fd4c233470df48c280d6745e25e1c35943834b
bbtemb <bbtemb@gmail.com>
2024-02-26 18:31:32 +0500

    Добавил усложненное 20 урока

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,17 +3,35 @@ const menu = () => {
   const menu = document.querySelector('menu');
   const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
+  const scrollBtn = document.querySelector('main>a');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
+  };
 
+  const smoothScroll = (item) => {
+    const itemHref = item.getAttribute('href').substr(1);
+
+    document.getElementById(itemHref).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   };
 
   menuBtn.addEventListener('click', handleMenu);
 
   closeBtn.addEventListener('click', handleMenu);
 
-  menuItems.forEach((menuItem) => menuItem.addEventListener('click', handleMenu));
+  scrollBtn.addEventListener('click', () => {
+    event.preventDefault();
+    smoothScroll(scrollBtn);
+  });
+
+  menuItems.forEach((menuItem) => menuItem.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleMenu();
+    smoothScroll(menuItem);
+  }));
 };
 
 export default menu;
