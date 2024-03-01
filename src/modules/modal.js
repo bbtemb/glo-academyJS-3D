1bda51833a4de371ead45551842e66c0d655ed43
bbtemb <bbtemb@gmail.com>
2024-03-02 01:04:53 +0500

    Добавил обязательное 25 урока

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,38 +1,24 @@
+import { animate } from './helpers';
+
 const modal = () => {
   const popup = document.querySelector('.popup');
   const modalBlock = document.querySelector('.popup-content');
   const buttons = document.querySelectorAll('.popup-btn');
 
-  const fadeIn = () => {
-    let opacity = 0;
-    const animate = () => {
-      opacity += 0.05;
-      popup.style.opacity = opacity;
-      if (opacity < 1) {
-        requestAnimationFrame(animate);
-      }
-    };
-    requestAnimationFrame(animate);
-  };
-  const transition = () => {
-    let x = -10;
-    modalBlock.style.left = `${x}%`;
-    const animate = () => {
-      x += 3;
-      modalBlock.style.left = `${x}%`;
-      if (x <= 38) {
-        requestAnimationFrame(animate);
-      }
-    };
-    requestAnimationFrame(animate);
-  };
-
   buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
       popup.style.display = 'block';
       if (screen.width >= 768) {
-        fadeIn();
-        transition();
+        animate({
+          duration: 400,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            popup.style.opacity = progress;
+            modalBlock.style.left = `${progress * 38}%`;
+          },
+        });
       }
     });
   });
@@ -42,6 +28,5 @@ const modal = () => {
       popup.style.display = 'none';
     }
   });
-
 };
 export default modal;
