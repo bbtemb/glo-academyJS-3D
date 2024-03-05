f861130b86b888e29162753aa5c9bba42bfaeec8
bbtemb <bbtemb@gmail.com>
2024-03-05 23:56:56 +0500

    добавил основное задание 27 урока

--- src/index.js
+++ src/index.js
@@ -5,11 +5,39 @@ import validation from './modules/validation';
 import tabs from './modules/tabs';
 import calc from './modules/calc';
 import slider from './modules/slider';
+import sendForm from './modules/sendForm';
 
-timer('5 march 2024');
+timer('10 march 2024');
 menu();
 modal();
 validation();
 tabs();
 calc(100);
 slider();
+sendForm({
+  formId: 'form1',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
+sendForm({
+  formId: 'form2',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
+sendForm({
+  formId: 'form3',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total',
+    },
+  ],
+});
